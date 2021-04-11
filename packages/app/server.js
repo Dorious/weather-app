be2af96f7e60e17c3a074064888f92f1442fa1e2
Dariusz Arciszewski <dorious@dorious.com>
2021-04-11 17:16:54 +0200

    chore(app): set hostname to the server

--- packages/app/server.js
+++ packages/app/server.js
@@ -3,6 +3,7 @@ const { parse } = require("url");
 const next = require("next");
 const fs = require("fs");
 const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -18,6 +19,6 @@ app.prepare().then(() => {
     handle(req, res, parsedUrl);
 }).listen(port, (err) => {
     if (err) throw err;
-    console.log("ready - started server on url: https://localhost:" + port);
+    console.log(`ready - started server on url: https://${host}:` + port);
   });
 });
