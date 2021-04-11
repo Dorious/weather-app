d100cae27119a1b159179e939be33185c78e1d44
Dariusz Arciszewski <dorious@dorious.com>
2021-04-11 22:00:42 +0200

    feat(app): search endpoint

---
+++ packages/app/pages/api/search.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { OpenCage } from '@dorious/weather-api'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { q } = req.query
+
+  const cityResponse = await new OpenCage.DefaultApi().vversionFormatGet(
+    1,
+    'json',
+    `${q}`,
+    'ce3818764ead4feea6be98e8c89f2d2d'
+  )
+
+  if (cityResponse.status === 200) {
+    const data = cityResponse.data.results
+    res.status(200).json({ data })
+  } else {
+    res.status(500).json({ message: 'Something went wrong!' })
+  }
+}
+
+export default handler
