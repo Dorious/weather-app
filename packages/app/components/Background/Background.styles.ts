55133d2745817de98b490352a8aa30e57d68e2a7
Dariusz Arciszewski <dorious@dorious.com>
2021-04-12 11:04:22 +0200

    fix(app): more backgrounds

--- packages/app/components/Background/Background.styles.ts
+++ packages/app/components/Background/Background.styles.ts
@@ -3,9 +3,15 @@ import styled from 'styled-components';
 import { AbsoluteDiv } from '../styles';
 
 function getLinearGradient(sky, daytime) {
+  const isClear = sky.match(/clearsky|fair/);
+  const isPartlyCloudy = sky.match(/partlycloud/);
   let colors = ['#777', '#ccc'];
 
-  if (daytime === 'night') {
+  if (isClear && daytime === 'day') {
+    colors = ['#4af', '#adf'];
+  } else if (isPartlyCloudy && daytime === 'day') {
+    colors = ['#aaa', '#adf'];
+  } else if (daytime === 'night') {
     colors = ['#001020', '#038'];
   }
 
