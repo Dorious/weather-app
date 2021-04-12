e97a789defb14a1fa784a36eea00f2a959d0ea8c
Dariusz Arciszewski <dorious@dorious.com>
2021-04-12 11:10:47 +0200

    fix(app): geolocation allow passing options

--- packages/app/utils/geolocation.ts
+++ packages/app/utils/geolocation.ts
@@ -2,16 +2,18 @@ import { SymbolCodes } from '../components/Symbol/Symbol';
 
 import legendJson from './legend.json';
 
-export const DEFAULT_TIMEOUT = 2000;
+export const DEFAULT_TIMEOUT = 5000;
+export const DEFAULT_MAX_AGE = 0;
 
 export function watchGeolocation(
   setPosition: (position: GeolocationPosition) => void,
-  setError: (position: GeolocationPositionError) => void
+  setError: (position: GeolocationPositionError) => void,
+  options: PositionOptions
 ): number {
   return navigator.geolocation.watchPosition(
     (position: GeolocationPosition) => setPosition(position),
     (positionError: GeolocationPositionError) => setError(positionError),
-    { timeout: DEFAULT_TIMEOUT }
+    { timeout: DEFAULT_TIMEOUT, maximumAge: DEFAULT_MAX_AGE, ...options }
   );
 }
 
