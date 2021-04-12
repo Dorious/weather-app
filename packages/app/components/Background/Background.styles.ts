71882900f3b88a1450fd783cb4de7d495b2bc66d
Dariusz Arciszewski <dorious@dorious.com>
2021-04-12 10:11:58 +0200

    feat(background): day/night background

--- packages/app/components/Background/Background.styles.ts
+++ packages/app/components/Background/Background.styles.ts
@@ -2,13 +2,25 @@ import styled from 'styled-components';
 
 import { AbsoluteDiv } from '../styles';
 
+function getLinearGradient(sky, daytime) {
+  let colors = ['#777', '#ccc'];
+
+  if (daytime === 'night') {
+    colors = ['#001020', '#038'];
+  }
+
+  return colors.join(', ');
+}
+
 export type StyledBackgroundProps = {
   sky: string;
   daytime: string;
 };
 
 export const Background = styled(AbsoluteDiv)<StyledBackgroundProps>`
-  background: #d1d1d1;
+  background: linear-gradient(
+    ${(props): string => getLinearGradient(props.sky, props.daytime)}
+  );
   z-index: -1;
   position: fixed;
 `;
